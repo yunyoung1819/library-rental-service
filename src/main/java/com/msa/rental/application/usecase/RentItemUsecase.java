c19149eccd2cefaf76158b6b1ca8a5633434d243
yunyoung1819 <yunyoung1819@hanmail.net>
2025-01-13 01:35:58 +0900

    :sparkles: feat: 애플리케이션 헥사곤 InputPort 구현

--- src/main/java/com/msa/rental/application/usecase/RentItemUsecase.java
+++ src/main/java/com/msa/rental/application/usecase/RentItemUsecase.java
@@ -1,6 +1,7 @@
 package com.msa.rental.application.usecase;
 
 import com.msa.rental.framework.web.dto.RentalCardOutputDTO;
+import com.msa.rental.framework.web.dto.UserItemInputDTO;
 
 public interface RentItemUsecase {
     public RentalCardOutputDTO rentItem(UserItemInputDTO rental) throws Exception;
