1626fb42fd403d95366bfa606c2645fb137b1411
yunyoung1819 <yunyoung1819@hanmail.net>
2025-01-27 16:30:51 +0900

    :sparkles: feat: "add BookMS and RentalMS"

--- src/main/java/com/msa/rental/application/usecase/ReturnItemUsecase.java
+++
@@ -1,8 +0,0 @@
-package com.msa.rental.application.usecase;
-
-import com.msa.rental.framework.web.dto.RentalCardOutputDTO;
-import com.msa.rental.framework.web.dto.UserItemInputDTO;
-
-public interface ReturnItemUsecase {
-    public RentalCardOutputDTO returnItem(UserItemInputDTO returnDto) throws Exception;
-}
